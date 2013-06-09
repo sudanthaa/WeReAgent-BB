0de3bf3bb775d5a5030ebe54c488455bf96462a0
Sudantha <sudanthaa@gmail.com>
2013-06-09 16:19:30 +0530

    Adding classpath file

--- src/smsks/wereagent/WRAScreen.java
+++ src/smsks/wereagent/WRAScreen.java
@@ -6,14 +6,7 @@ import net.rim.device.api.ui.component.ButtonField;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
 
-/**
- * A class extending the MainScreen class, which provides default standard
- * behavior for BlackBerry GUI applications.
- */
 public final class WRAScreen extends MainScreen {
-    /**
-     * Creates a new WRAScreen object
-     */
 	
 	WRAServer server = null;
 	ButtonField serviceButton = null;
@@ -27,10 +20,9 @@ public final class WRAScreen extends MainScreen {
         // Set the displayed title of the screen       
         setTitle("WeReAgent");
         
-        serviceButton = new ButtonField("Start");
+        serviceButton = new ButtonField("Start Server");
         serviceButton.setChangeListener(new FieldChangeListener() {
 			public void fieldChanged(Field field, int context) {
-				// TODO Auto-generated method stub
 				onPressServiceButton();
 			}
 		});
